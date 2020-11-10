bc81c4c03b3ed0a77e9cd62d99aca9c167ba0679
Deen_Git <sharmadeendayal39@gmail.com>
2020-11-10 11:42:08 +0530

    Employee.cs

---
+++ classes/Employee.cs
@@ -0,0 +1,13 @@
+﻿namespace DispatchTimerSampleInCS
+{
+    public class Employee
+    {
+        public int EmployeeId { get; set; }
+
+        public string EmpName { get; set; }
+
+        public string Qualification { get; set; }
+
+        public int Marks { get; set; }
+    }
+}
